daa697aef29c1f5d353726ccae72d52c30181674
Youcef <vcxw00@gmail.com>
2019-02-27 22:42:05 +0100

    Solved problem_two.py

--- problem_two.py
+++ problem_two.py
@@ -1,1 +1,10 @@
 """Write a program which can compute the factorial of a given numbers."""
+fact = 1
+while True:
+    a = int(input("Enter a number: "))
+    if a >= 0:
+        break
+if a != 0:
+    for x in range(1,a+1):
+        fact *= x
+print(fact)
