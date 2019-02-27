c326fc417d7c34dd3b100eb91ab05056fe4a39d8
Youcef <vcxw00@gmail.com>
2019-02-27 22:51:32 +0100

    Solved problem_three.py

--- problem_three.py
+++ problem_three.py
@@ -1,1 +1,13 @@
 """Write a program that solves second degree equation of the form ax^2 +bx + c = 0."""
+import math
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+c = int(input('Enter c: '))
+D = (b**2)-(4*a*c)
+if (D>0):
+    print('X1 = ',(-b-math.sqrt(D))/(2*a))
+    print('X2 = ',(-b+math.sqrt(D))/(2*a))
+elif (D==0):
+    print('X = ',(-b)/(2*a))
+else:
+    print("No solution")
