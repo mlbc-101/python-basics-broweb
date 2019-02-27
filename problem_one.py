4a0ccc05202f37e79e169f2d76635d47656d993a
Youcef <vcxw00@gmail.com>
2019-02-27 22:40:58 +0100

    Solved problem_one.py

--- problem_one.py
+++ problem_one.py
@@ -2,3 +2,6 @@
 Write a program which will find and display all such numbers which are divisible by 7 but are not a multiple of 5,
 between 2000 and 3200 (both included).
 """
+for x in range(2000, 3200):
+	if (x % 7 == 0) and not(x % 5 == 0):
+		print(x)
